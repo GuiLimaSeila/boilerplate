a78391a63819b74269af780e30a5ef62b3fe2089
GuiLimaSeila <guilhermelima@aluno.senai.br>
2024-03-13 15:05:17 -0300

    rn boiler

---
+++ src/screens/Home/index.jsx
@@ -0,0 +1,15 @@
+import { View } from 'react-native'
+import React from 'react'
+import styles from './styles'
+import Title from '../../components/Title'
+
+
+
+export default function Home() {
+
+  return (
+    <View style={styles.container}>
+      <Title title="Home" />
+    </View>
+  )
+}
